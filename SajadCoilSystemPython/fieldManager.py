0de69ae69dc08719bb4bfbb8241cf9955be965e1
microrobotical <40300827+microrobotical@users.noreply.github.com>
2018-06-15 13:00:25 -0400

    Document fieldManager.py
    Added some additional detail to the docstring and some comments here and there.

--- SajadCoilSystemPython/fieldManager.py
+++ SajadCoilSystemPython/fieldManager.py
@@ -1,15 +1,23 @@
 import numpy as np
 
 def field2signal(vectorField):
-    ''' a function that reads the inverse of characterization matrix of Sajad's coils '''
+    ''' 
+    A function that reads the inverse of characterization matrix of Sajad's coils from
+    a file and outputs the necessary coil currents for a desired magnetic field.
+    
+    The characterization matrix L is defined in the paper by Salmanipour and Diller 
+    "Eight-Degrees-of-Freedom Remote Actuation of Small Magnetic Mechanisms" ICRA 2018
+    as U = L*I where I is a vector of input coil currents and U is a vector containing 
+    the independent output magnetic field magnitude and gradient components.
+    '''
     np.set_printoptions(suppress=True)
+    # Load text values containing the matrix elements of L^-1 and reshape to 8x8
     fileName = 'InvActMatElements_new.txt'
     rawData = np.loadtxt(fileName)
     invMatrix = np.reshape(rawData,(8,8))
+    # I = (L^-1)*U
     signal = np.dot(invMatrix.transpose(),vectorField)/1000
     return signal
-
-
 
 class FieldManager(object):
     def __init__(self,dac):
@@ -20,6 +28,7 @@ class FieldManager(object):
     def setField(self,vecField):
         signal = field2signal(vecField)
         self.vecField = vecField
+        # Amplifier indices corresponding to the coil numbers defined by Sajad
         indexAmplifiers = np.matrix('2; 5; 4; 1; 3; 0; 6; 7')
         for i in range(8):
             self.dac.s826_aoPin(indexAmplifiers.item(i), signal[i])
