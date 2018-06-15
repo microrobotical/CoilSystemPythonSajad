76bc1128704e72927aafe5392d94c353641b5266
microrobotical <40300827+microrobotical@users.noreply.github.com>
2018-06-15 16:56:45 -0400

    Document fieldManager.py
    Mostly documentation changes, clarifications. Used np.matmul(a,b) instead of np.dot(a,b) for matrix multiplication. Removed unnecessary commented lines. Removed field2signal call from line 28 (the function returns a value, but there is no assignment on this line so it doesn't do anything useful).

--- SajadCoilSystemPython/fieldManager.py
+++ SajadCoilSystemPython/fieldManager.py
@@ -1,22 +1,31 @@
 import numpy as np
 
-# assign pin # to the coil
-# PIN_X1 = [0, 5.003] # pin number, factor number (mT/V)
-# PIN_X2 = [3, 4.879]
-# PIN_Y1 = [4, 5.143]
-# PIN_Y2 = [1, 5.024]
-# PIN_Z1 = [2, 5.024]
-# PIN_Z2 = [5, 4.433]
+# Amplifier gains from amplifier signal input in [V] to output in [A].
+#   currents = GAIN*signal
 GAIN = np.array([4.03, 3.44, 3.92, 3.90, 3.81, 3.95, 4.01, 4.00])
 
 
 def field2signal(vectorField):
-    ''' a function that reads the inverse of characterization matrix of Sajad's coils '''
+    ''' 
+    A function that reads the inverse of the characterization matrix L
+    of Sajad's coils from a file and returns the voltage signals to the
+    amplifiers given a desired vector field.
+    The 8x8 characterization matrix L is defined in the equation
+        vectorField = L*signal
+    where vectorField is a vector of the 8 independent magnetic field
+    values and signal is a vector of the voltage signals sent to each
+    of the 8 electromagnetic coil amplifiers. The voltage input signal
+    is directly proportional to the amplifier output current.
+    '''
     np.set_printoptions(suppress=True)
+    # Read the L^-1 values from a file and reshape them into an 8x8 matrix
     fileName = 'InvActMatElements_new.txt'
     rawData = np.loadtxt(fileName)
     invMatrix = np.reshape(rawData,(8,8))
-    signal = np.dot(invMatrix.transpose(),vectorField)/1000
+    # Note: Sajad recorded the values as col 1 followed by col 2, etc.
+    # Therefore the actual L^inv is the transpose of invMatrix
+    # Convert vectorField in mT and mT/m to T and T/m
+    signal = np.matmul(invMatrix.transpose(),vectorField/1000)
     return signal
 
 
@@ -25,13 +34,15 @@ class FieldManager(object):
     def __init__(self,dac):
         self.vecField = [0,0,0,0,0,0,0,0]
         self.dac = dac
-        field2signal([5,5,5,0,0,0,0,0])
 
     # Uniform field
     def setField(self,vecField):
         signal = field2signal(vecField)
         self.vecField = vecField
+        # Map the amplifiers 1-8 to the correct s826 pinouts
+        # e.g. Amplifier 2 corresponds to s826 Pin 5
         indexAmplifiers = np.matrix([[2],[5],[4],[1],[3],[0],[6],[7]])
+        # Output the signals from each pin
         for i in range(8):
             self.dac.s826_aoPin(indexAmplifiers.item(i), signal[i])
 
